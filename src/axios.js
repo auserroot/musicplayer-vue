50f7283ba5f322291dc6e2b1361fdc370f9bd827
auserroot <fauserfzy@gmail.com>
2021-05-21 18:14:37 +0800

    change base

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'https://apimusic.linweiqin.com',
+    // baseURL: 'https://apimusic.linweiqin.com',
+	baseURL:'http://rrafa.icu:3000',
 });
 export default instance;
