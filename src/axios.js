deb3d4dadc50fa62fabf52353afe36cdc69a473f
SuperAnt <fauserfzy@gmail.com>
2022-11-28 12:34:13 +0800

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
     // baseURL: 'https://apimusic.linweiqin.com',
-	baseURL:'http://rrafa.icu:3000',
+	baseURL:'http://cloud-music.pl-fe.cn/',
 });
 export default instance;
